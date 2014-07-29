eebeb1e176f1d09c628c6edb704ef1631aab4c6d
Ourai Lin <ourairyu@hotmail.com>
2014-07-29 10:01:05 +0800

    .inPage 兼容没启用 Turbolinks

--- src/coffee/adaptor.coffee
+++ src/coffee/adaptor.coffee
@@ -1,3 +1,40 @@
+turbolinksEnabled = @Turbolinks and @Turbolinks.supported is true
+
+pageHandlers = new Tatami.__class__.Storage "pageHandlers"
+
+handlerName = ( func ) ->
+  return func.toString().length.toString(16) + ""
+
+handlerExists = ( page, func ) ->
+  return @equal func, pageHandlers.get "#{page}.#{handlerName func}"
+
+addHandlers = ( page, func, isPlain ) ->
+  handlers = {}
+
+  @each page, ( flag, idx ) =>
+    if isPlain
+      func = flag
+      flag = idx
+
+    if not handlerExists.apply this, [flag, func]
+      name = handlerName func
+      
+      @namespace handlers, "#{flag}.#{name}"
+
+      handlers[flag][name] = func
+
+  pageHandlers.set handlers
+
+runHandlers = ( page, func, isPlain ) ->
+  @each page, ( flag, idx ) ->
+    if isPlain
+      func = flag
+      flag = idx
+
+    if $("body").is "[data-page='#{flag}']"
+      func()
+      return false
+
 Tatami.extend
   handlers: [
     {
@@ -10,26 +47,16 @@ Tatami.extend
         if arguments.length is 1 and not isObj
           page = [page] if not @isArray page
           result = @inArray($("body").data("page"), page) > -1
-        # 设置特定页面执行的函数
         else
           page = [page] if @isString page
-          handlers = {}
+          args = [page, func, isObj]
 
-          @each page, ( flag, idx ) =>
-            if isObj
-              func = flag
-              flag = idx
-
-            if not handlerExists.apply this, [flag, func]
-              name = handlerName func
-              
-              @namespace handlers, "#{flag}.#{name}"
-
-              handlers[flag][name] = func
-
-            return true
-
-          pageHandlers.set handlers
+          # 设置特定页面执行的函数
+          if turbolinksEnabled
+            addHandlers.apply this, args
+          # 立即执行函数
+          else
+            runHandlers.apply this, args
 
       validator: ( page, handler ) ->
         return @isString(page) or @isArray(page) or @isPlainObject(page)
@@ -37,23 +64,14 @@ Tatami.extend
       value: false
     }
   ]
-, Tatami
-
-pageHandlers = new Tatami.__class__.Storage "pageHandlers"
-
-handlerName = ( func ) ->
-  return func.toString().length.toString(16) + ""
-
-handlerExists = ( page, func ) ->
-  return @equal func, pageHandlers.get "#{page}.#{handlerName func}"
 
 # Support for turbolinks (https://github.com/rails/turbolinks)
-if @Turbolinks and @Turbolinks.supported is true
+if turbolinksEnabled
   Tatami.init
     runSandbox: ( prepareHandlers, readyHandlers ) ->
       $(document).on 
         "page:change": =>
-          @each pageHandlers.storage[$("body").attr("data-page")], ( handler ) ->
+          @each pageHandlers.storage[$("body").data("page")], ( handler ) ->
             handler()
 
           @run prepareHandlers
--- src/tatami-rails-adaptor.js
+++ src/tatami-rails-adaptor.js
@@ -1,5 +1,7 @@
 (function() {
-  var Adaptor, handlerExists, handlerName, pageHandlers;
+  var addHandlers, handlerExists, handlerName, pageHandlers, runHandlers, turbolinksEnabled;
+
+  turbolinksEnabled = this.Turbolinks && this.Turbolinks.supported === true;
 
   pageHandlers = new Tatami.__class__.Storage("pageHandlers");
 
@@ -11,12 +13,45 @@
     return this.equal(func, pageHandlers.get("" + page + "." + (handlerName(func))));
   };
 
-  Adaptor = {
+  addHandlers = function(page, func, isPlain) {
+    var handlers;
+    handlers = {};
+    this.each(page, (function(_this) {
+      return function(flag, idx) {
+        var name;
+        if (isPlain) {
+          func = flag;
+          flag = idx;
+        }
+        if (!handlerExists.apply(_this, [flag, func])) {
+          name = handlerName(func);
+          _this.namespace(handlers, "" + flag + "." + name);
+          return handlers[flag][name] = func;
+        }
+      };
+    })(this));
+    return pageHandlers.set(handlers);
+  };
+
+  runHandlers = function(page, func, isPlain) {
+    return this.each(page, function(flag, idx) {
+      if (isPlain) {
+        func = flag;
+        flag = idx;
+      }
+      if ($("body").is("[data-page='" + flag + "']")) {
+        func();
+        return false;
+      }
+    });
+  };
+
+  Tatami.extend({
     handlers: [
       {
         name: "inPage",
         handler: function(page, func) {
-          var handlers, isObj, result;
+          var args, isObj, result;
           isObj = this.isPlainObject(page);
           if (arguments.length === 1 && !isObj) {
             if (!this.isArray(page)) {
@@ -27,23 +62,12 @@
             if (this.isString(page)) {
               page = [page];
             }
-            handlers = {};
-            this.each(page, (function(_this) {
-              return function(flag, idx) {
-                var name;
-                if (isObj) {
-                  func = flag;
-                  flag = idx;
-                }
-                if (!handlerExists.apply(_this, [flag, func])) {
-                  name = handlerName(func);
-                  _this.namespace(handlers, "" + flag + "." + name);
-                  handlers[flag][name] = func;
-                }
-                return true;
-              };
-            })(this));
-            return pageHandlers.set(handlers);
+            args = [page, func, isObj];
+            if (turbolinksEnabled) {
+              return addHandlers.apply(this, args);
+            } else {
+              return runHandlers.apply(this, args);
+            }
           }
         },
         validator: function(page, handler) {
@@ -52,17 +76,15 @@
         value: false
       }
     ]
-  };
+  });
 
-  Tatami.extend(Adaptor, Tatami);
-
-  if (this.Turbolinks && this.Turbolinks.supported === true) {
+  if (turbolinksEnabled) {
     Tatami.init({
       runSandbox: function(prepareHandlers, readyHandlers) {
         return $(document).on({
           "page:change": (function(_this) {
             return function() {
-              _this.each(pageHandlers.storage[$("body").attr("data-page")], function(handler) {
+              _this.each(pageHandlers.storage[$("body").data("page")], function(handler) {
                 return handler();
               });
               return _this.run(prepareHandlers);
